bdedb9f2be7e9614854e0e8f3bd885cdccb1f41f
shreyavijayvargiya <shreyavijayvargiya16@gmail.com>
2020-10-20 23:17:18 +0530

    navigation_bar

--- app/src/main/java/com/dscnitp/freshersportal/MainActivity.java
+++ app/src/main/java/com/dscnitp/freshersportal/MainActivity.java
@@ -1,14 +1,56 @@
 package com.dscnitp.freshersportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_notes:
+                            openFragment(NotesFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_post:
+                            openFragment(PostFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_chat:
+                            openFragment(ChatFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_profile:
+                            openFragment(ProfileFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
