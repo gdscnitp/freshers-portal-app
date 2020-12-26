b23a23bcc6c6ccf2f804052f2550240b8fedce24
ANUJAGRAWAL2000 <anujagr2001@gmail.com>
2020-12-25 19:41:44 -0800

    Login_Registration_Complete

--- app/src/main/java/com/dscnitp/freshersportal/SplashScreen.java
+++ app/src/main/java/com/dscnitp/freshersportal/SplashScreen.java
@@ -32,8 +32,10 @@ public class SplashScreen extends AppCompatActivity {
     FirebaseUser user;
     FirebaseAuth firebaseAuth;
     private static int splash = 3000;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
