ce3d65c899bd5a5cba287213bfe7019887cd26ba
shreyavijayvargiya <shreyavijayvargiya16@gmail.com>
2020-12-30 00:10:30 +0530

    edit profile+ profile rating

--- app/src/main/java/com/dscnitp/freshersportal/Student/EditProfileActivity.java
+++ app/src/main/java/com/dscnitp/freshersportal/Student/EditProfileActivity.java
@@ -1,16 +1,35 @@
 package com.dscnitp.freshersportal.Student;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.dscnitp.freshersportal.R;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     Button update;
 
@@ -18,33 +37,56 @@ public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("users");
+
+
         update = (Button) findViewById(R.id.update);
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity();
+                Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+                query.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                        DatabaseReference myRef = database.getReference("name");
+                        TextInputEditText name = (TextInputEditText) findViewById(R.id.name);
+                        String text = name.getText().toString();
+                        myRef.setValue(text);
+
+                        DatabaseReference myRef1 = database.getReference("roll");
+                        TextInputEditText roll = (TextInputEditText) findViewById(R.id.roll);
+                        String text1 = roll.getText().toString().trim();
+                        myRef1.setValue(text1);
+
+                        DatabaseReference myRef2 = database.getReference("branch");
+                        TextInputEditText branch = (TextInputEditText) findViewById(R.id.branch);
+                        String text2 = branch.getText().toString();
+                        myRef2.setValue(text2);
+
+                        DatabaseReference myRef3 = database.getReference("year");
+                        TextInputEditText year = (TextInputEditText) findViewById(R.id.year);
+                        String text3 = year.getText().toString();
+                        myRef3.setValue(text3);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
             }
         });
 
-    }
 
-    public void openActivity() {
-        Intent intent = new Intent(this, ProfileFragment.class);
-        TextInputEditText name= (TextInputEditText) findViewById(R.id.name);
-        String text = name.getText().toString();
-        TextInputEditText roll= (TextInputEditText) findViewById(R.id.roll);
-        String text1 = roll.getText().toString();
-        TextInputEditText branch= (TextInputEditText) findViewById(R.id.branch);
-        String text2 = branch.getText().toString();
-        TextInputEditText year= (TextInputEditText) findViewById(R.id.year);
-        String text3 = year.getText().toString();
-
-        intent.putExtra("mytext", text);
-        intent.putExtra("mytext1",text1);
-        intent.putExtra("mytext2",text2);
-        intent.putExtra("mytext3",text3);
-
-        startActivity(intent);
     }
 }
-
--- app/src/main/java/com/dscnitp/freshersportal/Student/ProfileFragment.java
+++ app/src/main/java/com/dscnitp/freshersportal/Student/ProfileFragment.java
@@ -8,7 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -36,9 +38,11 @@ public class ProfileFragment extends Fragment {
 
 
     //views from xml
-    TextView nameTv, rollTv, branchTv;
+    TextView nameTv, rollTv, branchTv, yearTv ;
     ImageView logo;
-    Button editBtn;
+    Button editBtn, submit;
+    RatingBar ratingBar;
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -50,6 +54,37 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view= inflater.inflate(R.layout.fragment_profile, container, false);
+        ratingBar = (RatingBar) view.findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getActivity(), rating + "stars", Toast.LENGTH_SHORT).show();
+            }
+        });
+        submit = (Button) view.findViewById(R.id.submitBtn);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String totalStars = "Total Stars: " + ratingBar.getNumStars();
+                String rating = "Rating : " + ratingBar.getRating();
+                Toast.makeText(getActivity(), totalStars + "\n" + rating, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        //init firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        user= firebaseAuth.getCurrentUser();
+        firebaseDatabase= FirebaseDatabase.getInstance();
+        databaseReference= firebaseDatabase.getReference("users");
+
+        nameTv = view.findViewById(R.id.nameTv);
+        rollTv= view.findViewById(R.id.rollTv);
+        branchTv= view.findViewById(R.id.branchTv);
+        yearTv= view.findViewById(R.id.yearTv);
+
         editBtn = (Button) view.findViewById(R.id.editBtn);
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,15 +93,6 @@ public class ProfileFragment extends Fragment {
                 startActivity(intent);
             }
         });
-
-        //init firebase
-        firebaseAuth = FirebaseAuth.getInstance();
-        user= firebaseAuth.getCurrentUser();
-        firebaseDatabase= FirebaseDatabase.getInstance();
-        databaseReference=firebaseDatabase.getReference("users");
-
-        nameTv = view.findViewById(R.id.nameTv);
-
         //get info using signed in email of user
 
         Query query= databaseReference.orderByChild("email").equalTo(user.getEmail());
@@ -76,9 +102,16 @@ public class ProfileFragment extends Fragment {
 
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     String name = "" + ds.child("name").getValue();
+                    String roll = "" + ds.child("roll").getValue();
+                    String branch = "" + ds.child("branch").getValue();
+                    String year = "" + ds.child("year").getValue();
                     nameTv.setText(name);
+                    rollTv.setText(roll);
+                    branchTv.setText(branch);
+                    yearTv.setText(year);
 
-                }
+               }
+
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
