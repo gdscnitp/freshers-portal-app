957e707c90dc89843c55d5c6c3078a36c7529f3a
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-02-09 03:43:09 +0530

    ui updated

--- app/src/main/java/com/dscnitp/freshersportal/UserTypeActivity.java
+++ app/src/main/java/com/dscnitp/freshersportal/UserTypeActivity.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.dscnitp.freshersportal.Alumni.AlumniLoginActivity;
 import com.dscnitp.freshersportal.Student.LoginActivity;
 
 public class UserTypeActivity extends AppCompatActivity {
 
-    Button student,alumni;
+    LinearLayout student,alumni;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
